e1db69da5ddc01a3c0ee655e98a7f063fa0dd9c7
Ben_Farrell <ben.farrell08@gmail.com>
2020-06-14 18:15:39 +0100

    Adding Python Scripts to Automate ETL, Model Creation and Web Application Run

--- process_data.py
+++ process_data.py
@@ -40,8 +40,11 @@ def clean_data(df):
     return df
 
 def save_data(df, database_filename):
-    engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql(database_filename, engine, index=False)
+    engine = create_engine(f'sqlite:///{database_filename}')        
+    df.to_sql(database_filename.split('.db')[0],
+              con = engine,
+              index= False,
+              if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
