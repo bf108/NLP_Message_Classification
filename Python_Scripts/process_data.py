edd2562d9336c4fe1f283502f7d822895377c34d
Ben_Farrell <ben.farrell08@gmail.com>
2020-06-15 15:01:01 +0100

    Inlcuded doc string to functions

--- Python_Scripts/process_data.py
+++ Python_Scripts/process_data.py
@@ -3,14 +3,34 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    # load messages and categories dataset
-    messages = pd.read_csv('messages_filepath')
-    categories = pd.read_csv('categories_filepath')
+    '''
+    Merges message data and category data on id.
+    Outputs a single pandas DataFrame
 
-    # merge datasets
+    Args: full path to messages csv file, full path to categories csv file
+
+    Returns: Pandas DataFrame of combined
+
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
     return messages.merge(categories,on='id')
 
 def clean_data(df):
+    '''
+    Prepares raw data to train and test the model.
+
+    Splits each category into separate column and strips off all text to leave
+    binary value of 0, 1. 0 - Not related. 1 - Related.
+
+    Drops child alone category because there are no messages related to this
+    category. It would be a pointless exercise to train a model without data.
+
+    Args: Pandas DataFrame of raw data
+
+    Return: Pandas DataFrame of cleaned data ready for training model.
+
+    '''
     # create a dataframe of the 36 individual category columns
     categories = df.categories.str.split(pat=';',expand=True)
 
@@ -40,6 +60,15 @@ def clean_data(df):
     return df
 
 def save_data(df, database_filename):
+    '''
+    Saves pandas DataFrame to sqlite database
+
+    Args: Pandas DataFrame
+
+    Returns: None
+
+    '''
+
     engine = create_engine(f'sqlite:///{database_filename}')
     df.to_sql(database_filename.split('.db')[0],
               con = engine,
